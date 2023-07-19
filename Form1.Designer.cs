445c14a5b89baeba9cfa1ee0e9068436ad15976a
Jiradej Kerdsiri <106429190+ToonRider1983@users.noreply.github.com>
2023-07-19 14:12:42 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace SampleMySQL_DB {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.txtEmployeeSal = new System.Windows.Forms.TextBox();
+            this.txtEmployeeAddr = new System.Windows.Forms.TextBox();
+            this.txtEmployeePhone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgvEmployee = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(122, 382);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "บันทึก";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(203, 382);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "ล้าง";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(113, 57);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(84, 22);
+            this.txtEmployeeID.TabIndex = 2;
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(113, 91);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(109, 22);
+            this.txtEmployeeName.TabIndex = 3;
+            // 
+            // txtEmployeeSal
+            // 
+            this.txtEmployeeSal.Location = new System.Drawing.Point(324, 94);
+            this.txtEmployeeSal.Name = "txtEmployeeSal";
+            this.txtEmployeeSal.Size = new System.Drawing.Size(86, 22);
+            this.txtEmployeeSal.TabIndex = 4;
+            // 
+            // txtEmployeeAddr
+            // 
+            this.txtEmployeeAddr.Location = new System.Drawing.Point(113, 127);
+            this.txtEmployeeAddr.Name = "txtEmployeeAddr";
+            this.txtEmployeeAddr.Size = new System.Drawing.Size(297, 22);
+            this.txtEmployeeAddr.TabIndex = 5;
+            // 
+            // txtEmployeePhone
+            // 
+            this.txtEmployeePhone.Location = new System.Drawing.Point(113, 161);
+            this.txtEmployeePhone.Name = "txtEmployeePhone";
+            this.txtEmployeePhone.Size = new System.Drawing.Size(109, 22);
+            this.txtEmployeePhone.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ข้อมูลพนักงาน";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "รหัสพนักงาน";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ชื่อ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(247, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "เงินเดือน";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "ที่อยู่";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "เบอร์โทรศัพท์";
+            // 
+            // dgvEmployee
+            // 
+            this.dgvEmployee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployee.Location = new System.Drawing.Point(19, 199);
+            this.dgvEmployee.Name = "dgvEmployee";
+            this.dgvEmployee.RowHeadersWidth = 51;
+            this.dgvEmployee.RowTemplate.Height = 24;
+            this.dgvEmployee.Size = new System.Drawing.Size(699, 177);
+            this.dgvEmployee.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 418);
+            this.Controls.Add(this.dgvEmployee);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmployeePhone);
+            this.Controls.Add(this.txtEmployeeAddr);
+            this.Controls.Add(this.txtEmployeeSal);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.TextBox txtEmployeeSal;
+        private System.Windows.Forms.TextBox txtEmployeeAddr;
+        private System.Windows.Forms.TextBox txtEmployeePhone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgvEmployee;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SampleMySQL_DB {
+    public partial class Form1 : Form {
+
+        private MySqlConnection _connection;
+        private MySqlCommand _command;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool TestConnection()
+        {
+            this._connection = new MySqlConnection(
+                     "server=localhost; uid=root; database=samplemysql;"
+                ); //"server=127.0.0.1;uid=root;pwd=12345;database=test"
+
+            try
+            {
+                this._connection.Open();
+                this._connection.Close();
+                MessageBox.Show("Connection Successfull");
+                return true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Connection unsuccessfull");
+                return false;
+            }
+        }
+
+        private void FetchView()
+        {
+            MySqlCommand command = new MySqlCommand("SELECT * FROM employee_tbl");
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command.CommandText, _connection);
+            DataSet ds = new DataSet();
+
+            adapter.Fill(ds, "Employee_tbl");
+            dgvEmployee.DataSource = ds;
+            dgvEmployee.DataMember = "Employee_tbl";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.txtEmployeeID.Text = this.txtEmployeeName.Text = this.txtEmployeeSal.Text
+                = this.txtEmployeeAddr.Text = this.txtEmployeePhone.Text = "";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            this._command = new MySqlCommand(
+                $"INSERT INTO employee_tbl(eid, ename, salary, address, telephone) VALUES({this.txtEmployeeID.Text}" +
+                $", '{this.txtEmployeeName.Text}', {float.Parse(this.txtEmployeeSal.Text)}" +
+                $", '{this.txtEmployeeAddr.Text}', {this.txtEmployeePhone.Text});"
+                , _connection);
+
+            this._connection.Open();
+            this._command.ExecuteNonQuery();
+            this._connection.Close();
+            this.btnClear_Click(sender, e);
+            this.FetchView();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!this.TestConnection())
+            {
+                Application.Exit();
+            }
+            
+            this.FetchView();
+        }
+    }
+}
