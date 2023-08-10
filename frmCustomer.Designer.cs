b6d74f5de4606b84f0548627f58cf3937b5512e5
toonrider2526 <toonrider2526@gmail.com>
2023-08-10 22:08:18 +0700

    Update Lasted on 10-8-66 22.08

---
+++ frmCustomer.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace SampleMySQL_DB {
+    partial class frmCustomer {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.CheckBox();
+            this.btnEdit = new System.Windows.Forms.CheckBox();
+            this.dgvCustomer = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCustomerTel = new System.Windows.Forms.TextBox();
+            this.txtCustomerAddress = new System.Windows.Forms.TextBox();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCreditLimit = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCurrentBal = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnSearch.Location = new System.Drawing.Point(651, 168);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 29);
+            this.btnSearch.TabIndex = 31;
+            this.btnSearch.Text = "ค้นหา";
+            this.btnSearch.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnEdit.Location = new System.Drawing.Point(651, 133);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 29);
+            this.btnEdit.TabIndex = 30;
+            this.btnEdit.Text = "แก้ไข";
+            this.btnEdit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // dgvCustomer
+            // 
+            this.dgvCustomer.AllowUserToAddRows = false;
+            this.dgvCustomer.AllowUserToDeleteRows = false;
+            this.dgvCustomer.AllowUserToResizeRows = false;
+            this.dgvCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomer.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvCustomer.Location = new System.Drawing.Point(27, 248);
+            this.dgvCustomer.MultiSelect = false;
+            this.dgvCustomer.Name = "dgvCustomer";
+            this.dgvCustomer.ReadOnly = true;
+            this.dgvCustomer.RowHeadersWidth = 51;
+            this.dgvCustomer.RowTemplate.Height = 24;
+            this.dgvCustomer.Size = new System.Drawing.Size(699, 282);
+            this.dgvCustomer.TabIndex = 29;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 16);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "เบอร์โทรศัพท์";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 16);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "ที่อยู่";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 16);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "ชื่อ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "รหัสลูกค้า";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(20, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 25);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "ข้อมูลลูกค้า";
+            // 
+            // txtCustomerTel
+            // 
+            this.txtCustomerTel.Location = new System.Drawing.Point(130, 174);
+            this.txtCustomerTel.Name = "txtCustomerTel";
+            this.txtCustomerTel.Size = new System.Drawing.Size(109, 22);
+            this.txtCustomerTel.TabIndex = 22;
+            // 
+            // txtCustomerAddress
+            // 
+            this.txtCustomerAddress.Location = new System.Drawing.Point(130, 140);
+            this.txtCustomerAddress.Name = "txtCustomerAddress";
+            this.txtCustomerAddress.Size = new System.Drawing.Size(390, 22);
+            this.txtCustomerAddress.TabIndex = 21;
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(130, 104);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(109, 22);
+            this.txtCustomerName.TabIndex = 19;
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(130, 70);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(84, 22);
+            this.txtCustomerID.TabIndex = 18;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(651, 98);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 29);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "ล้าง";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(651, 67);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 29);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "บันทึก";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "จำกัดวงเงินที่";
+            // 
+            // txtCreditLimit
+            // 
+            this.txtCreditLimit.Location = new System.Drawing.Point(130, 211);
+            this.txtCreditLimit.Name = "txtCreditLimit";
+            this.txtCreditLimit.Size = new System.Drawing.Size(109, 22);
+            this.txtCreditLimit.TabIndex = 32;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(305, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 16);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "ยอดเงินคงเหลือ";
+            // 
+            // txtCurrentBal
+            // 
+            this.txtCurrentBal.Location = new System.Drawing.Point(411, 211);
+            this.txtCurrentBal.Name = "txtCurrentBal";
+            this.txtCurrentBal.Size = new System.Drawing.Size(109, 22);
+            this.txtCurrentBal.TabIndex = 34;
+            // 
+            // frmCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 552);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCurrentBal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCreditLimit);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.dgvCustomer);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCustomerTel);
+            this.Controls.Add(this.txtCustomerAddress);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.txtCustomerID);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Name = "frmCustomer";
+            this.Text = "Customer";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox btnSearch;
+        private System.Windows.Forms.CheckBox btnEdit;
+        private System.Windows.Forms.DataGridView dgvCustomer;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCustomerTel;
+        private System.Windows.Forms.TextBox txtCustomerAddress;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCreditLimit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCurrentBal;
+    }
+}
